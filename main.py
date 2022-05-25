4a1980a6f4965839c580bf358c88e745d48d2ee1
albuquerquealdry/bin/python3 <albuquerquealdry@gmail.com>
2022-05-24 21:29:23 -0300

    valt add

--- main.py
+++ main.py
@@ -7,34 +7,37 @@ import os
 import sys
 import json
 import base64
+import requests
 
-def getDefaultEnv():
-    envsPreprod = os.popen("""kubectl get secret default-env-preprod -o jsonpath='{.data}' """).read()
-    return json.loads(envsPreprod)
+def getVaultKey():
+    req = requests.get("http://127.0.0.1:8200/v1/envs/preprod",headers={'X-Vault-Token': 'hvs.2Uf0C9U7M0mPFYICcNarJtha'})
+    return req.content
 
 def getConfigMap():
-    with open("config_map_dev.yaml", 'r') as config_map:
-        configMap = yaml.load(config_map)
-        return configMap
+     with open("config_map_dev.yaml", 'r') as config_map:
+         configMap = yaml.load(config_map)
+         return configMap
    
 envs = ['HYBRIS_PORT', 'HYBRIS_HOST', 'SOLR_PORT' , 'SOLR_HOST', 'SOLR_SECURE', 'REDIS_PORT', 'REDIS_HOST', 'REDIS_JOAO'  ]
 for env in envs:
-    configMap = getConfigMap()
-    if env in configMap:
-        defaultEnvPreprod = getDefaultEnv()
+     configMap = getConfigMap()
+     if env in configMap:
+        defaultEnvPreprod = getVaultKey() 
+        dataJsonEnvsDefault = json.loads(defaultEnvPreprod)['data']
         valueConfigMap = configMap[env]['preprod']
-        valueDefault = base64.b64decode(defaultEnvPreprod[env])
-        os.system(f"sed -i 's,{valueConfigMap},{valueDefault.decode('utf-8')},g' config_map_dev.yaml ")
-    else:
-        print(f"NOT FOUND ENV {env}")
+        valueDefault = dataJsonEnvsDefault[env]
+
+        os.system(f"sed -i 's,{valueConfigMap},{valueDefault},g' config_map_dev.yaml ")
+     else:
+         print(f"NOT FOUND ENV {env}")
 
 
-dicEnvs = getConfigMap()
-envsList = list(getConfigMap())
+# dicEnvs = getConfigMap()
+# envsList = list(getConfigMap())
 
-for env in envsList:   
-    try:
-        dicEnvs[env]['preprod']
-    except KeyError:
-        print(f"a env {env} nao existe para preprod")
-        dicEnvs[env].apend()
+# for env in envsList:   
+#      try:
+#          dicEnvs[env]['preprod']
+#      except KeyError:
+#          print(f"a env {env} nao existe para preprod")
+#          dicEnvs[env].apend()
